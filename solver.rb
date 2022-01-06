9f0212052fd2e92d77bce0b8b9040c69d06555e5
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-07 02:00:29 +0430

    Add fizzbuzz function

--- solver.rb
+++ solver.rb
@@ -1,20 +1,33 @@
 class Solver
-    def factorial(int)
-      if int < 0
-        raise StandardError.new "Error, only integers bigger than 0 are permitted"
-      end
-      out = 1
-      while int > 0
-        out = out * int
-        int = int - 1
-      end
-      return out
-    end
+  def factorial(int)
+    raise StandardError, 'Error, only integers bigger than 0 are permitted' if int.negative?
 
-    def reverse(input) 
-      splited = input.split('')
-      reverse = ''
-      splited.each {|char| reverse = char + reverse}
-      return reverse.to_str
+    out = 1
+    while int.positive?
+      out *= int
+      int -= 1
     end
+    out
+  end
+
+  def reverse(input)
+    splited = input.chars
+    reverse = ''
+    splited.each { |char| reverse = char + reverse }
+    reverse.to_str
+  end
+
+  def fizzbuzz(input)
+    d_by_five = input % 5
+    d_by_three = input % 3
+    if d_by_three === 0 && d_by_five.zero?
+      'fizzbuzz'
+    elsif d_by_five === 0
+      'buzz'
+    elsif d_by_three === 0
+      'fizz'
+    else
+      input.to_s
+    end
+  end
 end
