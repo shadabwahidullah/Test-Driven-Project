1e9ef8d0feb8000a5f226a0db6e9680f240f2da0
KatIsCoding <fabrigoru@gmail.com>
2022-01-06 15:11:27 -0600

    Add Factorial Solution

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
 class Solver
-    
+    def factorial(int)
+      if int < 0
+        raise StandardError.new "Error, only integers bigger than 0 are permitted"
+      end
+      out = 1
+      while int > 0
+        out = out * int
+        int = int - 1
+      end
+      return out
+    end
 end
