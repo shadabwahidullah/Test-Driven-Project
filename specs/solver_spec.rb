5de2db16e42451e83dc776f5e9692cc81f472736
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-07 01:24:27 +0430

    Add test for solver and factorial function

---
+++ specs/solver_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../solver'
+
+describe Solver do
+    before(:each) do
+        @i_solver = Solver.new
+    end
+    context 'Instantiate solver' do
+        it '#new' do
+            solver = Solver.new
+            expect(solver).to be_a Solver
+        end
+    end
+
+    context 'Test factorial function' do
+        it 'Factorial function receives a positive integer' do
+            expect(@i_solver.factorial(5)).to eq (120)
+        end
+        it 'Factorial fucntion receives zero' do
+            expect(@i_solver.factorial(0)).to eq (1)
+        end
+        it 'Factorial fucntion receives a negative number' do
+            expect(@i_solver.factorial(-1)).to raise_error
+        end
+    end
+end
